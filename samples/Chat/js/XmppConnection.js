0e68dbd239236546c316f801b894fc448abe2b75
J.T <gisfromscratch@live.de>
2015-05-13 23:38:54 +0200

    Added the strophe library and a simple connection class

---
+++ samples/Chat/js/XmppConnection.js
@@ -0,0 +1,41 @@
+﻿/**
+ * A simple XMPP connection.
+ */
+define(["dojo/_base/declare"], function (declare) {
+    return declare(null, {
+
+        _user: "",
+        _pass: "",
+        _url: "http://esprimo:7070/http-bind/ ",
+
+        _connection: undefined,
+
+        connect: function (user, pass) {
+            if (this._connection) {
+                return;
+            }
+
+            this._connection = new Strophe.Connection(this._url, { sync: false });
+            this._connection.connect(user, pass, this.onStatusChanged);
+        },
+
+        disconnect: function () {
+            if (!this._connection) {
+                return;
+            }
+
+            this._connection.disconnect(this.onStatusChanged);
+            delete this._connection;
+        },
+
+        onStatusChanged: function(status) {
+            switch (status) {
+                case Strophe.Status.CONNECTED:
+                    break;
+
+                case Strophe.Status.DISCONNECTED:
+                    break;
+            }
+        }
+    });
+});
