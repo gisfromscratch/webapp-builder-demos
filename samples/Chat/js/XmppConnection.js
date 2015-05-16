0bb816a37b6dcc0091d41f208b661956b65a695b
J.T <gisfromscratch@live.de>
2015-05-16 23:03:36 +0200

    Added a presence handler and sending the default presence showing the user as online

--- samples/Chat/js/XmppConnection.js
+++ samples/Chat/js/XmppConnection.js
@@ -7,6 +7,9 @@ define(["dojo/_base/declare", "dojo/_base/lang", "dojo/topic"], function (declar
         _url: "http://esprimo:7070/http-bind/ ",
 
         _connection: null,
+        _connected: false,
+
+        _handlers: [],
 
         connectedEvent: "connected",
         disconnectedEvent: "disconnected",
@@ -29,14 +32,43 @@ define(["dojo/_base/declare", "dojo/_base/lang", "dojo/topic"], function (declar
             delete this._connection;
         },
 
+        sendMessage: function(message) {
+            if (!this._connection || !this._connected) {
+                return;
+            }
+
+            // TODO: Send the message
+        },
+
+        onPresenceChanged: function(presence) {
+            // TODO: Handle presence of users
+        },
+
         onStatusChanged: function(status) {
             switch (status) {
                 case Strophe.Status.CONNECTED:
+                    this._connected = true;
                     topic.publish(this.connectedEvent);
+
+                    // Add a handler and send the default presence
+                    // User will shown as online
+                    var handler = this._connection.addHandler(dojo.hitch(this, this.onPresenceChanged));
+                    this._handlers.push(handler);
+                    var presence = $pres();
+                    this._connection.send(presence);
                     break;
 
                 case Strophe.Status.DISCONNECTED:
+                    this._connected = false;
                     topic.publish(this.disconnectedEvent);
+
+                    // Delete all handlers
+                    var handler;
+                    for (var index in this._handlers) {
+                        handler = this._handlers[index];
+                        this._connection.deleteHandler(handler);
+                    }
+                    this._handlers = [];
                     break;
             }
         }
